369c5b8d860b9d57cc5892062f79e2f4a9585d6f
apav-dev <apavlick@yext.com>
2024-09-06 14:51:14 -0400

    remove location template

--- src/ve.config.tsx
+++ src/ve.config.tsx
@@ -1,81 +1,13 @@
 import type { Config } from "@measured/puck";
-import { HeroComponent as Hero, HeroProps } from "./components/Hero";
 import {
   HeroComponent as FinProHero,
   HeroProps as FinProHeroProps,
 } from "./components/financial-professional/FinProHero";
-import {
-  ColumnsComponent as Columns,
-  ColumnsProps,
-} from "./components/Columns";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { CardComponent as Card, CardProps } from "./components/Card";
-import {
-  StoreInfoCardComponent as StoreInfoCard,
-  StoreInfoCardProps,
-} from "./components/StoreInfoCard";
-import { FAQComponent as FAQ, FAQProps } from "./components/FAQ";
-import {
-  DeliveryPromoComponent as DeliveryPromo,
-  DeliveryPromoProps,
-} from "./components/DeliveryPromo";
-import { BannerComponent as Banner, BannerProps } from "./components/Banner";
-import {
-  HoursCardComponent as HoursCard,
-  HoursCardProps,
-} from "./components/HoursCard";
-import { PromoComponent as Promo, PromoProps } from "./components/Promo";
-import {
-  FeaturedItemsComponent as FeaturedItems,
-  FeaturedItemsProps,
-} from "./components/FeaturedItems";
 
 import "@yext/visual-editor/style.css";
 
-type LocationProps = {
-  Hero: HeroProps;
-  Columns: ColumnsProps;
-  Card: CardProps;
-  FAQ: FAQProps;
-  DeliveryPromo: DeliveryPromoProps;
-  Banner: BannerProps;
-  HoursCard: HoursCardProps;
-  StoreInfoCard: StoreInfoCardProps;
-  Promo: PromoProps;
-  FeaturedItems: FeaturedItemsProps;
-};
-
 type FinancialProfessionalProps = {
   FinProHero: FinProHeroProps;
-};
-
-// All the available components for locations
-export const locationConfig: Config<LocationProps> = {
-  components: {
-    Hero,
-    Columns,
-    Card,
-    FAQ,
-    DeliveryPromo,
-    Banner,
-    HoursCard,
-    StoreInfoCard,
-    Promo,
-    FeaturedItems,
-  },
-  root: {
-    render: ({ children }) => {
-      return (
-        <>
-          <Header />
-          {children}
-          <Footer />
-        </>
-      );
-    },
-    fields: {},
-  },
 };
 
 export const financialProfessionalConfig: Config<FinancialProfessionalProps> = {
@@ -91,6 +23,5 @@ export const financialProfessionalConfig: Config<FinancialProfessionalProps> = {
 };
 
 export const componentRegistry = new Map<string, Config<any>>([
-  ["location", locationConfig],
   ["Financial Professional", financialProfessionalConfig],
 ]);
